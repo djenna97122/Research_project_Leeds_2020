0713901dda320a88a4538c9722f38b8e9cf818c4
djenna97122 <63618874+djenna97122@users.noreply.github.com>
2020-06-30 19:36:48 +0200

    amoc features

--- global_summaries.r
+++ global_summaries.r
@@ -81,3 +81,91 @@ nc_close(nc_file)
 nc_file<-nc_open("deglg.vn1_0.temp_uo.mean.ANN.001")
 tmp_array<-ncvar_get(nc_file,"temp_uo.mean")
 head(tmp_array)
+
+
+################## AMOC###################
+
+############# Read data #############
+setwd("/Volumes/annie/earpal/database/experiments/deglg/vn1_0/merid_Atlantic_ym_dpth/001")
+filename <- "deglg.vn1_0.merid_Atlantic_ym_dpth.annual.ANN.001.nc"
+data<-nc_open(filename)
+print(data)
+dname<-names(data$var)
+tmp_array<-ncvar_get(data,dname,start=c(1,1,1),count=c(-1,-1,5000))
+############# Get the variable & attributes #############
+lon<-ncvar_get(data, "longitude")
+m_v<-ncatt_get( data, dname, "missing_value", verbose=FALSE)
+v_max<-ncatt_get(data, dname, "valid_max")
+v_min<-ncatt_get(data, dname, "valid_min")
+sbmdl<-ncatt_get( data, dname, "submodel")
+s_c<-ncatt_get(data, dname, "stash_code")
+F_V<-ncatt_get(data, dname, "_FillValue")
+p<-ncatt_get(data, dname, "processing")
+pcmdi_name<-ncatt_get( data, dname,"pcmdi_name" )
+pp_name<-ncatt_get( data, dname, "pp_name")
+std_name<-ncatt_get( data, dname, "standard_name")
+ln<-ncatt_get( data, dname, "long_name")
+time<-ncatt_get( data, dname, "time")
+t <- ncvar_get(data, "t")
+head(t)
+dim(t)
+t1<-t[1:5000]
+summary(t1)
+tim=1
+############# Get the maxamoc long #############
+
+amoc_mean<-t1
+
+for (tim in 1:dim(t1)){
+  vt<-tmp_array[ , ,tim]#matrix lon = rows, lat= col
+
+  data_mean<-apply(vt,2,max,na.rm=TRUE)
+  length(data_mean)
+  data_mean
+  head(vt)
+  data_mean<-weighted.mean(as.matrix(data_mean),w,na.rm=TRUE)
+  sst_mean[tim]<-data_mean
+};
+sst_mean
+plot(t1,sst_mean,type='l',main="deglg.ANN.001 -sea surface temperature vs time ")
+#plot(t1,temps_mean_icecon,type='l',main="deglg.ANN.001 -ice concentration vs time ")
+
+############# Create the new files #############
+
+setwd("/Volumes/annie/earded")
+dim_new<-ncdim_def("t",units="year",vals=t1,create_dimvar=TRUE, calendar="360_day") #create time dimension 
+var<-ncvar_def("sst.mean",units="K",dim=dim_new) #create the variable: table with the global summaries
+nc_file<-nc_create("deglg.vn1_0.sst.mean.ANN.001.nc",var) #create the ncdef files
+print(nc_file) 
+ncvar_put(nc_file,"sst.mean",sst_mean) #upload the values in the table 
+ncvar_get(nc_file,"sst.mean")
+############# Create all attributes #############
+ncatt_put(nc_file,"sst.mean","missing_value",m_v$value)
+ncatt_put(nc_file,"sst.mean","valid_max",-200)
+ncatt_put(nc_file,"sst.mean","valid_min",200)
+ncatt_put(nc_file,"sst.mean","submodel",sbmdl$value)
+ncatt_put(nc_file,"sst.mean","stash_code",s_c$value)
+ncatt_put(nc_file,"sst.mean","_FillValue",F_V$value)
+ncatt_put(nc_file,"sst.mean","processing",p$value)
+ncatt_put(nc_file,"sst.mean","pcmdi_name",pcmdi_name$value)
+ncatt_put(nc_file,"sst.mean","pp_name",pp_name$value)
+ncatt_put(nc_file,"sst.mean","standard_name",std_name$value)
+ncatt_put(nc_file,"sst.mean","long_name",ln$value)
+ncatt_put(nc_file,"sst.mean","time",time$value)
+ncatt_put(nc_file,"sst.mean","title",ln$value)
+ncatt_put(nc_file,"sst.mean","name","sst.mean")
+print(nc_file)
+nc_close(nc_file)
+nc_file<-nc_open("deglg.vn1_0.sst.mean.ANN.001.nc")
+tmp_array2<-ncvar_get(nc_file,"sst.mean")
+(tmp_array2)
+
+
+
+nc_close(nc_file)
+
+
+
+
+
+
